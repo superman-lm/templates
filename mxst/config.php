ad78fb30196db2fc7f58b3b3ad367705beca6200
sunhaiyang <s_hy@live.cn>
2013-12-03 18:04:39 +0800

    init

---
+++ mxst/config.php
@@ -0,0 +1,20 @@
+<?php return array (
+  'name' => 'mxst',
+  'disable' => 0,
+  'dirname' => 'mxst',
+  'file_explan' => 
+  array (
+    'templates|mxst|content' => 
+    array (
+      'category_download.html' => '',
+      'download.html' => '',
+      'footer.html' => '',
+      'header.html' => '',
+      'index.html' => '',
+      'list_download.html' => '',
+      'list_sx.html' => '筛选页',
+      'show_dl.html' => '下载页',
+      'show_download.html' => '',
+    ),
+  ),
+);?>
